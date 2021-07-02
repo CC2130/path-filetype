273a3f9fa320895e5fc559ab57fcc6c178f3964a
Jia Chao <jiachao2130@126.com>
2021-07-02 22:57:26 +0800

    release 0.1.1

--- README.md
+++ README.md
@@ -5,6 +5,18 @@ If you don't know chinese, the next may give you a chance to learn chinses.
   
 它提供了一个特性`PathFileType`，使你可以直接在使用 Path 类型时，通过 p.filetype()
 来获得此路径的文件类型（万物皆文件）。  
+```rust
+#[cfg(unix)]
+pub enum FileType {
+    Regular,
+    Directory,
+    Symlink,
+    CharDevice,
+    BlockDevice,
+    Fifo,
+    Socket,
+}
+```
   
 其只支持 Uinx 类系统，因为目前我只在 Linux 上有使用的需要，后续将继续完善，添加
 常用系统，如 MacOS、Windows 的支持。  
@@ -31,7 +43,9 @@ let _regular_file = Path::new("data/regular_file");
 assert_eq!("entity not found", format!("{}", _not_exist.filetype().unwrap_err()));
 
 // 判断是否为普通文件
-assert_eq!(_regular_file.filetype().unwrap(), FileType::Regular);
+if let Ok(regular_file) = _regular_file.filetype() {
+    assert_eq!(regular_file, FileType::Regular);
+}
 
 // 判断是否为目录
 assert_eq!(_dir.filetype().unwrap(), FileType::Directory);
@@ -50,3 +64,33 @@ assert_eq!(_dir.filetype().unwrap(), FileType::Directory);
     let _ = remove_file(_symlink_file);
 }
 ```
+  
+此外，亦可直接对期望类型进行判断，提供 `is_symlink()`、`is_char_device()`、
+`is_block_device()`、`is_fifo()`、`is_socket()`方法，返回值为`bool`：  
+```rust
+extern crate path_filetype;
+
+use std::path::Path;
+
+use path_filetype::*;
+
+#[cfg(unix)]
+{
+    use std::os::unix::fs;
+    use std::fs::remove_file;
+
+    let _symlink_file = Path::new("data/symlink_file");
+    let _ = fs::symlink("regular_file", _symlink_file);
+
+    assert_eq!(_symlink_file.is_symlink(), true);
+
+    // Path 中原提供的`is_file()`和`is_dir()`方法不能判断链接
+    assert_eq!(_symlink_file.is_file(), true);
+
+    let _ = remove_file(_symlink_file);
+}
+```
+## 注意
+下面几项，在使用时需要注意一下：  
+ * 推荐使用 `if let Ok(_filetype) = _path.filetype() { // actions }`方式。
+ * `Path`中原有的`is_file()`和`is_dir()`方法，不能用来判断路径是否为链接文件。
--- src/unix.rs
+++ src/unix.rs
@@ -22,7 +22,23 @@ pub enum Regular {
 }
 
 pub trait PathFileType {
+    /// 返回一个枚举类型结果 FileType
     fn filetype(&self) -> io::Result<FileType>;
+
+    /// 判断路径是否为一个链接
+    fn is_symlink(&self) -> bool;
+
+    /// 判断路径是否为字符设备文件
+    fn is_char_device(&self) -> bool;
+
+    /// 判断路径是否为块设备文件
+    fn is_block_device(&self) -> bool;
+
+    /// 判断路径是否为管道文件
+    fn is_fifo(&self) -> bool;
+
+    /// 判断路径是否为套接字文件
+    fn is_socket(&self) -> bool;
 }
 
 impl PathFileType for Path {
@@ -32,30 +48,66 @@ impl PathFileType for Path {
             return Err(io::Error::from(ErrorKind::NotFound))
         }
 
-        // 使用 symlink_metadata 方法才可判断其是否为链接文件
-        let _filetype = self.symlink_metadata().unwrap().file_type();
-        println!("{}", _filetype.is_symlink());
-
-        if _filetype.is_block_device() {
+        if self.is_block_device() {
             Ok(FileType::BlockDevice) 
-        } else if _filetype.is_char_device() {
+        } else if self.is_char_device() {
             Ok(FileType::CharDevice)
-        } else if _filetype.is_fifo() {
+        } else if self.is_fifo() {
             Ok(FileType::Fifo)
-        } else if _filetype.is_socket() {
+        } else if self.is_socket() {
             Ok(FileType::Socket)
-        } else if _filetype.is_symlink() {
+        } else if self.is_symlink() {
             Ok(FileType::Symlink)
-        } else if _filetype.is_dir() {
+        } else if self.is_dir() {
             Ok(FileType::Directory)
         } else {
             Ok(FileType::Regular)
         }
+    }
+
+    fn is_symlink(&self) -> bool {
+        self.symlink_metadata().unwrap().file_type().is_symlink()
+    }
+
+    fn is_char_device(&self) -> bool {
+        self.metadata().unwrap().file_type().is_char_device()
+    }
+
+    fn is_block_device(&self) -> bool {
+        self.metadata().unwrap().file_type().is_block_device()
+    }
+
+    fn is_fifo(&self) -> bool {
+        self.metadata().unwrap().file_type().is_fifo()
+    }
+
+    fn is_socket(&self) -> bool {
+        self.metadata().unwrap().file_type().is_socket()
     }
 }
 
 impl PathFileType for PathBuf {
     fn filetype(&self) -> io::Result<FileType> {
         self.as_path().filetype()
+    }
+
+    fn is_symlink(&self) -> bool {
+        self.as_path().is_symlink()
+    }
+
+    fn is_char_device(&self) -> bool {
+        self.as_path().is_char_device()
+    }
+
+    fn is_block_device(&self) -> bool {
+        self.as_path().is_block_device()
+    }
+
+    fn is_fifo(&self) -> bool {
+        self.as_path().is_fifo()
+    }
+
+    fn is_socket(&self) -> bool {
+        self.as_path().is_socket()
     }
 }
